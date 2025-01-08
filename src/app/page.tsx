4a1781e9ba3be1a29f4ef515a858b4c5d50a36f6
Dhaval Jotaneeya <jotaneeya@gmail.com>
2025-01-08 16:42:54 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,8 @@ export default function Home() {
     '2024-Navratri-Dashera-Mandli',
     '2024-Navratri-Day-8',
     '2024-Navratri-Day-5',
-    '2024-Navratri-Day-4'
+    '2024-Navratri-Day-4',
+    'Darshna-AI'
   ];
 
 return (

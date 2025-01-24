e5123fbcc1549746463132af7c9cadb2a3c2ffbe
Dhaval Jotaneeya <jotaneeya@gmail.com>
2025-01-24 10:08:55 +0530

    Update [repo].js

--- pages/[repo].js
+++ pages/[repo].js
@@ -11,13 +11,14 @@ const Gallery = ({ images, repo }) => {
           <a
             key={index}
             href={image.fullUrl}
+            style={{cursor: 'pointer'}} // Add pointer cursor visually
             download // This forces the download
           >
             <img
               src={image.thumbnailUrl}
               alt={`Thumbnail ${index}`}
               loading="lazy"
-              style={{cursor: 'pointer'}} // Add pointer cursor visually
+              
             />
           </a>
         ))}

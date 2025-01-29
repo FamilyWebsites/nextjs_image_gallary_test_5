dc873f485f8c4e531f8749ec2605775d1284b27f
DhavalJotaneeya <jotaneeya@gmail.com>
2025-01-29 17:36:08 +0530

    adding image

--- pages/[repo].js
+++ pages/[repo].js
@@ -28,34 +28,8 @@ const Gallery = ({ images, repo }) => {
 
 // Fetch the list of repositories for static paths
 export async function getStaticPaths() {
-  const repos = [
-    'adival-prewedding',
-    '2023-Engagement-party',
-    '2023-My-Birthday-Celebration-in-Office',
-    '2023-Ravimamas-marriage',
-    '2023-Mummy-Pappa-Marrige-Album',
-    '2023-Uttrayan',
-    '2023-Valentine-s-Day',
-    '2023-SGL-Late-night-working-TCS-Project',
-    '2023-Meeting-with-Ridhdhi',
-    '2023-Exatra-photos-with-Aditi',
-    '2023-Push-in-boots-the-last-wish',
-    '2023-Keval-s-Tour',
-    'Shreya-s-Farwell',
-    'navratri-2024',
-    'Aditi-With-Her-Friends-Stree-2',
-    '2022-Adival-Kankupagla',
-    '2024-Navratri-Dashera-Mandli',
-    '2024-Navratri-Day-8',
-    '2024-Navratri-Day-5',
-    '2024-Navratri-Day-4',
-    'Darshna-AI-new',
-    'manali-merriage-photos',
-    '2025-hungrito-food-fest-7.0',
-    '2024-Merriage-photos-2',
-    'Binalben-na-Marriage',
-    '2025-Manjukaki-Harikaka-50th-anniversary'
-  ];
+  const repos = [test-git-1---Copy,
+  'test-git-1---Copy---Copy'];
 
   const paths = repos.map(repo => ({ params: { repo } }));
 

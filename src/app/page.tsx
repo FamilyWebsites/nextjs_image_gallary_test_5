3f8b957cd59883a4af499fab071cba9cf2ef192d
Dhaval Jotaneeya <jotaneeya@gmail.com>
2025-01-29 19:58:09 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,8 +26,7 @@ export default function Home() {
     '2024-Merriage-photos-2',
     'Binalben-na-Marriage',
     '2025-Manjukaki-Harikaka-50th-anniversary'
-  ,
-  'test-git-1---Copy---Copy'];
+  ];
 
 return (
     <main className="flex min-h-screen flex-col items-center justify-center p-6">
